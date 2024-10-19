f829ece3771a54ce468dd3c47e0b5f181b595861
Erik Jensen <erik@appulent.co.uk>
2024-10-19 12:13:52 +0100

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import CopyPlugin from 'copy-webpack-plugin';
+
+const nextConfig = {
+    webpack: (config, {}) => {
+        config.plugins.push(
+          new CopyPlugin({
+            patterns: [
+              { 
+                from: 'node_modules/govuk-frontend/dist/govuk/assets',
+                to: '../public/assets'
+              }
+            ]
+          })
+        )
+        return config
+      }
+};
 
 export default nextConfig;
